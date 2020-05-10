80d66812c58fddd0b5fe0db414a5193eada1c4e6
Michael Havey - Fitzgerald <michaelhavey-fitzgerald@michaels-mbp.mynet>
2020-05-10 19:14:58 +0100

    feat: allow user to customise mask placement

--- website/src/components/ImageMasker.vue
+++ website/src/components/ImageMasker.vue
@@ -2,10 +2,71 @@
 	<div class="ImageMasker">
 		<h3 class="ImageMasker__title">Final Adjustments:</h3>
 
-		<canvas
-			ref="canvas"
-			:width="imageSize"
-			:height="imageSize"/>
+		<section class="ImageMasker__content">
+
+			<canvas
+				class="ImageMasker__canvas"
+				ref="canvas"
+				:width="imageSize"
+				:height="imageSize"/>
+
+			<section class="ImageMasker__controls">
+
+				<b-form-group
+					label="Left Offset"
+					label-for="leftOffset">
+					<b-form-input
+						id="leftOffset"
+						v-model="maskSizing.leftOffset"
+						type="range"
+						min="0"
+						max="1"
+						step="0.05"
+						@input="onValueChange"/>
+				</b-form-group>
+
+				<b-form-group
+					label="Top Offset"
+					label-for="topOffset">
+					<b-form-input
+						id="topOffset"
+						v-model="maskSizing.topOffset"
+						type="range"
+						min="0"
+						max="1"
+						step="0.05"
+						@input="onValueChange"/>
+				</b-form-group>
+
+				<b-form-group
+					label="Width"
+					label-for="width">
+					<b-form-input
+						id="width"
+						v-model="maskSizing.width"
+						type="range"
+						min="0"
+						max="1"
+						step="0.05"
+						@input="onValueChange"/>
+				</b-form-group>
+
+				<b-form-group
+					label="Height"
+					label-for="height">
+					<b-form-input
+						id="height"
+						v-model="maskSizing.height"
+						type="range"
+						min="0"
+						max="1"
+						step="0.05"
+						@input="onValueChange"/>
+				</b-form-group>
+
+			</section>
+
+		</section>
 
 		<!-- The following elements are not visible to the user -->
 		<img
@@ -33,7 +94,7 @@ export default {
 			required: true
 		},
 		url: {
-			type: String,
+			// type: String,
 			required: true
 		},
 		data: {
@@ -44,54 +105,56 @@ export default {
 	data() {
 		return {
 			context: null,
-			originalDrawn: false
+			originalDrawn: false,
+			maskSizing: {
+				leftOffset: 0.3,
+				topOffset: 0.4,
+				width: 0.5,
+				height: 0.2
+			}
 		};
 	},
 	methods: {
-		setupOriginal() {
-			this.$refs.original.onload = () => {
-				this.context.drawImage(
-					this.$refs.original,
-					0,
-					0,
-					this.imageSize,
-					this.imageSize
-				);
-				this.originalDrawn = true;
-			};
+		drawOriginal() {
+			this.context.drawImage(
+				this.$refs.original,
+				0,
+				0,
+				this.imageSize,
+				this.imageSize
+			);
 		},
-		addMask(mask) {
-			const defaults = {
-				leftOffsetScale: 0.3,
-				topOffsetScale: 0.4,
-				widthScale: 0.5,
-				heightScale: 0.2
-			};
-			mask.onload = async () => {
-				// try every half second for 10 seconds, until image is loaded
-				const retries = 20;
-				const pollTime = 500;
-				for (let i = 0; i < retries; i++) {
-					await sleep(pollTime); // eslint-disable-line no-await-in-loop
-					if (this.originalDrawn) {
-						this.context.drawImage(
-							mask,
-							this.imageSize * defaults.leftOffsetScale,
-							this.imageSize * defaults.topOffsetScale,
-							this.imageSize * defaults.widthScale,
-							this.imageSize * defaults.heightScale
-						);
-						break;
-					}
-				}
-				throw new Error('Error adding mask to original, original was not drawn.');
-			};
+		drawMask(mask) {
+			this.context.drawImage(
+				this.$refs[mask],
+				this.imageSize * this.maskSizing.leftOffset,
+				this.imageSize * this.maskSizing.topOffset,
+				this.imageSize * this.maskSizing.width,
+				this.imageSize * this.maskSizing.height
+			);
+		},
+		onValueChange() {
+			this.context.clearRect(0, 0, this.imageSize, this.imageSize);
+			this.drawOriginal();
+			this.drawMask('mask1');
 		}
 	},
-	mounted() {
+	async mounted() {
 		this.context = this.$refs.canvas.getContext('2d');
-		this.setupOriginal();
-		this.addMask(this.$refs.mask1);
+		this.$refs.original.onload = () => {
+			this.drawOriginal();
+			this.originalDrawn = true;
+		};
+		// try every half second for 10 seconds, until image is loaded
+		const retries = 20;
+		const pollTime = 500;
+		for (let i = 0; i < retries; i++) {
+			await sleep(pollTime); // eslint-disable-line no-await-in-loop
+			if (this.originalDrawn) {
+				return this.drawMask('mask1');
+			}
+		}
+		throw new Error('Error adding mask to original, original was not drawn.');
 	}
 };
 
@@ -107,6 +170,22 @@ $spacing: 2rem;
 	&__title {
 		color: $primary;
 		margin-bottom: $spacing;
+	}
+
+	&__content {
+		display: flex;
+		flex-direction: row;
+	}
+
+	&__canvas {
+		width: 50%;
+	}
+
+	&__controls {
+		display: flex;
+		flex-direction: column;
+		width: 50%;
+		padding: 1rem;
 	}
 
 	&__hidden {
