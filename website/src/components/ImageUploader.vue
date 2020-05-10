aa7c99cbaf5f0779a087e023df3dec08fcdf37ff
Michael Havey - Fitzgerald <michaelhavey-fitzgerald@michaels-mbp.mynet>
2020-05-10 13:27:07 +0100

    feat: user can now add image to page from disk

---
+++ website/src/components/ImageUploader.vue
@@ -0,0 +1,55 @@
+<template>
+	<div class="ImageUploader">
+		<h3 class="ImageUploader__title">Upload an image</h3>
+		<image-uploader
+			class="ImageUploader__uploader"
+			:debug="1"
+			:maxWidth="512"
+			:maxHeight="512"
+			:quality="0.9"
+			:autoRotate=true
+			outputFormat="verbose"
+			:preview=true
+			:capture="true"
+			doNotResize="['gif', 'svg']"
+			@onUpload="startImageResize"
+			@onComplete="endImageResize"/>
+	</div>
+</template>
+
+<script>
+import ImageUploader from 'vue-image-upload-resize';
+
+export default {
+	components: {
+		ImageUploader
+	},
+	methods: {
+
+	}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+@import '@holistic-web/el-layout/src/styles/theme';
+
+.ImageUploader {
+	display: flex;
+	flex-direction: column;
+
+	&__title {
+		color: $primary;
+		margin-bottom: 1rem;
+	}
+
+	&__uploader {
+
+		.img-preview {
+			margin-right: 1rem;
+		}
+
+	}
+}
+
+</style>
---
+++ website/src/components/Maskify.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="Maskify">
+
+		<p class="Maskify__summary">
+			A key tool in our fight against the coronavirus...
+		</p>
+
+		<image-uploader/>
+	</div>
+</template>
+
+<script>
+import ImageUploader from './ImageUploader.vue';
+
+export default {
+	components: {
+		ImageUploader
+	},
+	data() {
+		return {
+			progress: {
+				imageAdded: false
+			}
+		};
+	}
+};
+</script>
+
+<style lang="scss">
+
+.Maskify {
+	width: 85%;
+	margin: 2rem 7.5%;
+
+	&__summary {
+		margin-bottom: 2rem;
+	}
+}
+</style>
