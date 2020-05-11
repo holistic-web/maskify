10a3afca3378e95f4f36311143d7f33d4d165cf1
Andrew Lewis <andrew12lewis@gmail.com>
2020-05-11 00:32:28 -0700

    feat: detect mouth centre for overlay

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -1,4 +1,26 @@
 const functions = require('firebase-functions');
 
-exports.getFacialRecognitionData = functions.https.onCall(url => `I can't find a face in ${url}`);
-// TODO make a call to google vision API with image url
+const vision = require('@google-cloud/vision');
+
+const client = new vision.ImageAnnotatorClient();
+
+exports.getFacialRecognitionData = functions.https.onCall(async url => {
+	try {
+		const [result] = await client.faceDetection(`gs://cloud-samples-data/vision/face/faces.jpeg`);
+		const faces = result.faceAnnotations;
+		let x = 0;
+		let y = 0;
+		faces.forEach((face, i) => {
+			face.landmarks.forEach((landmark, j) => {
+				if (landmark.type === 'MOUTH_CENTER') {
+					x = landmark.position.x;
+					y = landmark.position.y;
+					return (x, y);
+				}
+			});
+		});
+	} catch (err) {
+		console.log('err', err);
+		console.log(`I can't find a face in ${url}`);
+	}
+});
