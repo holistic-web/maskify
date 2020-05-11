8725b1f7b44f3e6b62b3b76640855761a26858ed
Andrew Lewis <andrew12lewis@gmail.com>
2020-05-11 11:55:25 -0700

    chore: update func

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -6,19 +6,8 @@ const client = new vision.ImageAnnotatorClient();
 
 exports.getFacialRecognitionData = functions.https.onCall(async url => {
 	try {
-		const [result] = await client.faceDetection(`gs://cloud-samples-data/vision/face/faces.jpeg`);
-		const faces = result.faceAnnotations;
-		let x = 0;
-		let y = 0;
-		faces.forEach((face, i) => {
-			face.landmarks.forEach((landmark, j) => {
-				if (landmark.type === 'MOUTH_CENTER') {
-					x = landmark.position.x;
-					y = landmark.position.y;
-					return (x, y);
-				}
-			});
-		});
+		const result = await client.faceDetection(url);
+		console.log(Object.keys(result));
 	} catch (err) {
 		console.log('err', err);
 		console.log(`I can't find a face in ${url}`);
