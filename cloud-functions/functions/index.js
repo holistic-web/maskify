d8b4778dce552ddcb538289afef9e5223301e710
GitHub Action <action@github.com>
2020-07-20 22:31:58 +0000

    automated linting fix-ups

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -1,15 +1,15 @@
-const functions = require("firebase-functions");
-const Vision = require("@google-cloud/vision");
+const functions = require('firebase-functions');
+const Vision = require('@google-cloud/vision');
 
 const vision = new Vision.ImageAnnotatorClient();
 
 exports.getFacialRecognitionData = functions.https.onCall(
-  async (base64Image) => {
-    try {
-      const [result] = await vision.faceDetection(base64Image);
-      return result;
-    } catch (err) {
-      return `Error in cloud function: ${err}`;
-    }
-  }
+	async base64Image => {
+		try {
+			const [result] = await vision.faceDetection(base64Image);
+			return result;
+		} catch (err) {
+			return `Error in cloud function: ${err}`;
+		}
+	}
 );
